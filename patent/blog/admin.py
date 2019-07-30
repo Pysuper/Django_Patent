b5ebb0b56f60836c8fe37128a62410d23b267749
pysuper <1821764535@qq.com>
2019-07-30 14:17:19 +0800

    在同一页面编辑管理数据, 定制site

--- patent/blog/admin.py
+++ patent/blog/admin.py
@@ -1,28 +1,13 @@
+from django.urls import reverse
 from django.contrib import admin
 
-# Register your models here.
-from django.urls import reverse
+from patent.custom_site import custom_site
+from .adminforms import PostAdminForm
+from .models import Post, Category, Tag
 from django.utils.html import format_html
 
-from .models import Post, Category, Tag
 
-
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ("name", "status", "is_nav", "created_time", "post_count")  # 这里显示的是修改数据之后后台可以看到的页面中的数据
-    fields = ("name", "status", "is_nav")  # 这是用户添加数据的时候可以看到的页面
-
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user  # 把当前登录的用户赋值给obj.owner ==> 如果未登录通过request.user拿到匿名用户对象
-        return super(CategoryAdmin, self).save_model(request, obj, form, change)  # change用来保存当前数据是新增的还是更新的
-
-    def post_count(self, obj):
-        return obj.post_set.count()
-
-    post_count.short_description = "文章数量"
-
-
-@admin.register(Tag)
+@admin.register(Tag, site=custom_site)
 class TagAdmin(admin.ModelAdmin):
     list_display = ("name", "status", "created_time")
     fields = ("name", "status")
@@ -47,8 +32,9 @@ class CategoryOwnerFilter(admin.SimpleListFilter):
         return queryset
 
 
-@admin.register(Post)
+@admin.register(Post, site=custom_site)
 class PostAdmin(admin.ModelAdmin):
+    form = PostAdminForm
     list_display = ["title", "category", "status", "created_time", "owner", "operator"]
     list_display_links = []  # 配置哪些字段可以作为链接, 点击他们进入编辑页面
     list_filter = [CategoryOwnerFilter]  # 配置页面过滤器, 需要通过哪些字段来过滤列表
@@ -66,7 +52,7 @@ class PostAdmin(admin.ModelAdmin):
     #     "content",
     #     "tag",
     # )
-    fieldsets = (
+    fieldsets = (  # 用来控制布局 ==> (当前模块的名称, {内容:当前模块的描述, 字段和样式配置}),
         (
             "基础配置", {
                 "description": "基础配置描述",
@@ -90,7 +76,8 @@ class PostAdmin(admin.ModelAdmin):
     )
 
     def operator(self, obj):  # 自定义函数的参数是一定的就是当前 行 的对象, 列表中的每一行对应数据库表中的一条数据, 也对应Model中的一个实例
-        return format_html("<a href='{}'>编辑</a>", reverse("admin:blog_post_change", args=(obj.id,)))
+        # return format_html("<a href='{}'>编辑</a>", reverse("admin:blog_post_change", args=(obj.id,)))
+        return format_html("<a href='{}'>编辑</a>", reverse("cus_admin:blog_post_change", args=(obj.id,)))
 
     operator.short_description = "操作"
 
@@ -101,3 +88,33 @@ class PostAdmin(admin.ModelAdmin):
     def get_queryset(self, request):
         qs = super(PostAdmin, self).get_queryset(request)
         return qs.filter(owner=request.user)
+
+    # 自定义导入静态资源 ==> 可以通过Media() 类来向页面中添加想要的Javascript和CSS资源
+    class Media():
+        css = {
+            "all": ("https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"),
+        }
+        js = ("https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/bootstrap.bundle.js")
+
+
+# 在页面中编辑关联数据
+class PostInline(admin.TabularInline):  # StackedInline  样式不同
+    fields = ('title', 'desc')
+    extra = 1  # 控制额外多几个
+    model = Post
+
+
+@admin.register(Category, site=custom_site)
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [PostInline, ]
+    list_display = ("name", "status", "is_nav", "created_time", "post_count")  # 这里显示的是修改数据之后后台可以看到的页面中的数据
+    fields = ("name", "status", "is_nav")  # 这是用户添加数据的时候可以看到的页面
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user  # 把当前登录的用户赋值给obj.owner ==> 如果未登录通过request.user拿到匿名用户对象
+        return super(CategoryAdmin, self).save_model(request, obj, form, change)  # change用来保存当前数据是新增的还是更新的
+
+    def post_count(self, obj):
+        return obj.post_set.count()
+
+    post_count.short_description = "文章数量"
