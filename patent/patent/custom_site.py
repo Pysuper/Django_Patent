e151cf90970551e4f194217c54b5374538fb1449
pysuper <1821764535@qq.com>
2019-07-30 14:21:19 +0800

    定制site

--- patent/patent/custom_site.py
+++ patent/patent/custom_site.py
@@ -2,6 +2,11 @@ from django.contrib.admin import AdminSite
 
 
 class CustomSite(AdminSite):
+    """
+    定制site:
+        把 `用户管理模块` 和 ` 文章分类数据的管理` 分开
+        修改后台的默认展示 (Django站点管理后台) ==> SmallSpider
+    """
     site_header = 'SmallSpider'
     site_title = 'SmallSpider管理后台'
     index_title = '首页'
